a4b85902713c17c1ef5a41ffcecb6d08469bc6bc
Marisa <marisa.malveiro1@gmail.com>
2025-01-25 01:02:55 +0000

    FireBase teste

--- pdm2projeto/app/src/main/java/com/example/pdm2_projeto/MainActivity.java
+++ pdm2projeto/app/src/main/java/com/example/pdm2_projeto/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.pdm2_projeto;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +9,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        // TESTE Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "João");
+        user.put("email", "joao@example.com");
+        user.put("age", 25);
+
+        db.collection("users")
+                .add(user)
+                .addOnSuccessListener(documentReference ->
+                        Log.d("Firestore", "Documento criado com ID: " + documentReference.getId())
+                )
+                .addOnFailureListener(e ->
+                        Log.w("Firestore", "Erro ao criar documento", e)
+                );
     }
 }
